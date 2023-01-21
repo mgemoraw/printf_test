967cb62594f69b89259fa5f9268f9418731fee09
mgemoraw <meng.get4@gmail.com>
2023-01-21 20:15:07 +0300

    new files added

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char c);
+unsigned int _strlen(char *fmt);
+int _printf(const char * format, ...);
+
+#endif
